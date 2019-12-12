12a5378e18c8e93330e478195f9dec4a0aec18e4
Alexrodriguez2090 <54908770+Alexrodriguez2090@users.noreply.github.com>
2019-12-11 20:30:35 -0600

    Adding files

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+
 #Recreating mustang
 @app.route('/contactparsing')
 def contactparsing():
@@ -33,7 +34,12 @@ def postData():
 		json.dump(jsdata, f)
 	return print("Contacts saved")
 
+
 #Recipe websites
+@app.route('/recipebookIndex')
+def recipebookIndex():
+	return render_template('recipebookindex.html')
+
 @app.route('/recipes', methods=["GET", "POST"])
 def recipes():
 	root = os.path.realpath(os.path.dirname(__file__))
@@ -57,12 +63,9 @@ def recipes():
 			olddata = json.load(open(data))
 			entry = olddata[int(request.form['update'])]
 			entrynumber = int(request.form['update'])
-			#print(entry, file=sys.stderr)
 			entryname = entry['name']
 			entryinstr = entry['instr']
 			entryingr = entry['ingr']
-			#with open(data, 'w') as f:
-				#json.dump(olddata, f)
 			return updatePage(entryname, entryinstr, entryingr, entrynumber)
 		elif 'name' in request.form:
 			root = os.path.realpath(os.path.dirname(__file__))
@@ -86,11 +89,152 @@ def recipes():
 			return redirect(url_for('recipes'))
 
 	else:
-		return render_template("recipes.html", recipebook=recipes, type="Home")
+		return render_template("recipes.html", recipebook=recipes, type="Meat")
+
+@app.route('/souprecipes', methods=["GET", "POST"])
+def soupRecipes():
+	root = os.path.realpath(os.path.dirname(__file__))
+	data = os.path.join(root, 'data', 'souprecipes.json')
+	recipes = json.load(open(data))
+
+	if request.method == "POST":
+		if 'delete' in request.form:
+			root = os.path.realpath(os.path.dirname(__file__))
+			data = os.path.join(root, 'data', 'souprecipes.json')
+
+			olddata = json.load(open(data))
+			del olddata[int(request.form['delete'])]
+			with open(data, 'w') as f:
+				json.dump(olddata, f)
+			return redirect(url_for('souprecipes'))
+		elif 'update' in request.form:
+			root = os.path.realpath(os.path.dirname(__file__))
+			data = os.path.join(root, 'data', 'souprecipes.json')
+
+			olddata = json.load(open(data))
+			entry = olddata[int(request.form['update'])]
+			entrynumber = int(request.form['update'])
+			entryname = entry['name']
+			entryinstr = entry['instr']
+			entryingr = entry['ingr']
+			return updatePage(entryname, entryinstr, entryingr, entrynumber)
+		elif 'name' in request.form:
+			root = os.path.realpath(os.path.dirname(__file__))
+			data = os.path.join(root, 'data', 'souprecipes.json')
+
+			olddata = json.load(open(data))
+			entrynumber = int(request.form['entrynumber'])
+			entry = olddata[entrynumber]
+			entryname = entry['name']
+			entryinstr = entry['instr']
+			entryingr = entry['ingr']
+
+			newname = request.form['name']
+			newingr = request.form['ingr']
+			newinstr = request.form['instr']
+			entry['name'] = request.form['name']
+			entry['ingr'] = request.form['ingr']
+			entry['instr'] = request.form['instr']
+			with open(data, 'w') as f:
+				json.dump(olddata, f)
+			return redirect(url_for('souprecipes'))
+
+	else:
+		return render_template("recipes.html", recipebook=recipes, type="Soups")
+
+@app.route('/vegetarianrecipes', methods=["GET", "POST"])
+def vegetarianRecipes():
+	root = os.path.realpath(os.path.dirname(__file__))
+	data = os.path.join(root, 'data', 'vegetarianrecipes.json')
+	recipes = json.load(open(data))
+
+	if request.method == "POST":
+		if 'delete' in request.form:
+			root = os.path.realpath(os.path.dirname(__file__))
+			data = os.path.join(root, 'data', 'vegetarianrecipes.json')
+
+			olddata = json.load(open(data))
+			del olddata[int(request.form['delete'])]
+			with open(data, 'w') as f:
+				json.dump(olddata, f)
+			return redirect(url_for('vegetarianRecipes'))
+		elif 'update' in request.form:
+			root = os.path.realpath(os.path.dirname(__file__))
+			data = os.path.join(root, 'data', 'vegetarianrecipes.json')
+
+			olddata = json.load(open(data))
+			entry = olddata[int(request.form['update'])]
+			entrynumber = int(request.form['update'])
+			entryname = entry['name']
+			entryinstr = entry['instr']
+			entryingr = entry['ingr']
+			return updatePage(entryname, entryinstr, entryingr, entrynumber)
+		elif 'name' in request.form:
+			root = os.path.realpath(os.path.dirname(__file__))
+			data = os.path.join(root, 'data', 'vegetarianrecipes.json')
+
+			olddata = json.load(open(data))
+			entrynumber = int(request.form['entrynumber'])
+			entry = olddata[entrynumber]
+			entryname = entry['name']
+			entryinstr = entry['instr']
+			entryingr = entry['ingr']
+
+			newname = request.form['name']
+			newingr = request.form['ingr']
+			newinstr = request.form['instr']
+			entry['name'] = request.form['name']
+			entry['ingr'] = request.form['ingr']
+			entry['instr'] = request.form['instr']
+			with open(data, 'w') as f:
+				json.dump(olddata, f)
+			return redirect(url_for('vegetarianRecipes'))
+
+	else:
+		return render_template("recipes.html", recipebook=recipes, type="Vegetarian")
 
 @app.route('/updateentry', methods=["GET", "POST"])
 def updatePage(entryname, entryinstr, entryingr, entrynumber):
 	return render_template("updatepage.html", entryname=entryname, entryingr=entryingr, entryinstr=entryinstr, entrynumber=entrynumber)
+
+@app.route('/newrecipe', methods=["GET", "POST"])
+def newRecipe():
+	if request.method == "POST":
+		nameVar = request.form['name']
+		ingrVar = request.form['ingr']
+		instrVar = request.form['instr']
+		recipeType = request.form['recipeType']
+		newList = dict(ingr=ingrVar, instr=instrVar, name=nameVar)
+
+		if recipeType == "meat":
+			root = os.path.realpath(os.path.dirname(__file__))
+			data = os.path.join(root, 'data', 'meatrecipes.json')
+
+			olddata = json.load(open(data))
+			olddata.append(newList)
+			with open(data, 'w') as f:
+				json.dump(olddata, f)
+			return redirect(url_for('recipes'))
+		elif recipeType == "soup":
+			root = os.path.realpath(os.path.dirname(__file__))
+			data = os.path.join(root, 'data', 'souprecipes.json')
+
+			olddata = json.load(open(data))
+			olddata.append(newList)
+			with open(data, 'w') as f:
+				json.dump(olddata, f)
+			return redirect(url_for('soupRecipes'))
+		elif recipeType == "vegetarian":
+			root = os.path.realpath(os.path.dirname(__file__))
+			data = os.path.join(root, 'data', 'vegetarianrecipes.json')
+
+			olddata = json.load(open(data))
+			olddata.append(newList)
+			with open(data, 'w') as f:
+				json.dump(olddata, f)
+			return redirect(url_for('vegetarianRecipes'))
+	else:
+		return render_template("newrecipe.html")
 
 #Debug
 if __name__ == '__main__':
